9ef4874eb049a2d8dc893e602c9d4f1e1fce38c7
Kyle Wilcox <kyle@axiomalaska.com>
2015-04-13 12:48:48 -0400

    Mongo port needs to be an int

--- larva_library/production.py
+++ larva_library/production.py
@@ -4,5 +4,5 @@ DEBUG = True
 
 # Database
 MONGODB_HOST = os.environ.get('MONGODB_HOST')
-MONGODB_PORT = os.environ.get('MONGODB_PORT')
+MONGODB_PORT = int(os.environ.get('MONGODB_PORT'))
 MONGODB_DATABASE = os.environ.get('MONGODB_DATABASE')
